aab93faa82782c3930648043d41659de1bdd8bdf
dkozak94 <dkozak94@gmail.com>
2016-02-16 07:03:18 +0100

    add ll gramatic to syntactic.php

--- syntactic.php
+++ syntactic.php
@@ -15,3 +15,28 @@ function syntactic_analysis(){
     }
     return 1;
 }
+
+/*
+
+S->(START_TWO)
+
+START_TWO->{STATES},{ALPHABET},{RULES},{START},{FINISH}
+
+STATES -> state STATES_N
+STATES_N -> epsilon
+STATES_N -> ,state STATES_N
+
+ALPHABET -> symbol ALPHABET_N
+ALPHABET_N -> epsilon
+ALPHABET_N -> ,symbol ALPHABET_N
+
+FINISH -> state FINISH_N
+FINISH_N -> epsilon
+FINISH_N -> ,state FINISH_N
+
+RULES -> RULE RULUES_N
+RULE -> state symbol -> state
+RULES_N -> epsilon
+RULES_N -> ,RULE RULES_N
+
+*/
